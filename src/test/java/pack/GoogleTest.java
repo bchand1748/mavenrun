4f596d3ea786d6753d4133f5f09f960b464ed1a7
bchand1748 <bchand@nova.edu>
2015-09-15 12:41:33 -0400

    Signed-off-by: bchand1748 <bchand@nova.edu>

--- src/test/java/pack/GoogleTest.java
+++ src/test/java/pack/GoogleTest.java
@@ -1,15 +1,30 @@
 package pack;
 
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
-
+	
+		
 	@Test
 	
 	public void openGoogle()
 	{
-		FirefoxDriver driver =new FirefoxDriver();
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\bkbc011\\Desktop\\chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
 		driver.get("http://www.bk.com");
+	    driver.quit();
+	     
+	
 	}
+
+
+
 }
