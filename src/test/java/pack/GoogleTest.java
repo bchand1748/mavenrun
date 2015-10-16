9e46eeee6a8e3f55722dc61d98a1b35491ff1c05
bchand1748 <bchand@nova.edu>
2015-10-16 11:15:31 -0400

    Signed-off-by: bchand1748 <bchand@nova.edu>

--- src/test/java/pack/GoogleTest.java
+++ src/test/java/pack/GoogleTest.java
@@ -14,13 +14,16 @@ public class GoogleTest {
 		
 	@Test
 	
-	public void openGoogle()
+	public void openGoogle() throws InterruptedException
 	{
 		
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\bkbc011\\Desktop\\chromedriver.exe");
 		 WebDriver driver = new ChromeDriver();
 		driver.get("http://www.bk.com");
-	    driver.quit();
+	    
+		Thread.sleep(7000);
+		
+		driver.quit();
 	     
 	//tracking changes  
 	}
